30c243cf00be0863908c71e4436b079a4061149d
QuangDev1998 <leminhquangdev@gmail.com>
2024-09-28 17:35:15 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import Products from "./Pages/Products/Products";
+
+function App() {
+  return (
+    <div className="App">
+     <Products />
+    </div>
+  );
+}
+
+export default App;
